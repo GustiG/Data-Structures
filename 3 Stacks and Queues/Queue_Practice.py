e4c4e3dc90d116408c3b8d51eb1864d189c55550
GustiG <augustin_gurgulescu@yahoo.com>
2024-07-20 17:04:23 +0300

    Implementantion practice

--- 3 Stacks and Queues/Queue_Practice.py
+++ 3 Stacks and Queues/Queue_Practice.py
@@ -210,7 +210,7 @@ q.print_q()
 ###############################################################
 ###############################################################
 ###############################################################
-
+'''
 class Node:
     def __init__(self, value):
         self.value = value
@@ -259,7 +259,55 @@ q.enqueue(3)
 q.dequeue()
 q.print_queue()
 
+'''
 
+###############################################################
+###############################################################
+###############################################################
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node    = Node(value)
+        self.first  = new_node
+        self.last   = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp:
+            print(f'{temp.value}', end=" ")
+            temp = temp.next
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.first = new_node
+            self.last  = new_node
+        else:
+            self.last.next = new_node
+            self.last      = new_node
+        self.length += 1
+
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp         = self.first
+        self.first   = self.first.next
+        temp.next    = None
+        self.length -= 1
+
+q = Queue(0)
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.dequeue()
+q.print_queue()
 
 ###############################################################
 ###############################################################
