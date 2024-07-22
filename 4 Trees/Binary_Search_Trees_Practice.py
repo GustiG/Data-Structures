08a8be682da2ce0464ac29bb1ed19e6ccb354b7c
GustiG <augustin_gurgulescu@yahoo.com>
2024-07-22 22:32:49 +0300

    Binary Search Tree implementation practice

--- 4 Trees/Binary_Search_Trees_Practice.py
+++ 4 Trees/Binary_Search_Trees_Practice.py
@@ -770,7 +770,7 @@ print(bst.contains(1))
 ##############################################################
 ##############################################################
 ##############################################################
-
+'''
 class Node:
     def __init__(self, value):
         self.value = value
@@ -820,3 +820,58 @@ bst.insert(2)
 bst.insert(8)
 print(bst.root.right.right.value)
 print(bst.contains(8))
+'''
+##############################################################
+##############################################################
+##############################################################
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False            
+            elif new_node.value < temp.value:
+                if not temp.left:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if not temp.right:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+    def contains(self, value):
+        temp = self.root
+        while temp:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+    
+bst = BinarySearchTree()
+bst.insert(3)
+bst.insert(2)
+bst.insert(6)
+print(bst.root.left.value)
+print(bst.root.right.value)
+print(bst.contains(2))
+
