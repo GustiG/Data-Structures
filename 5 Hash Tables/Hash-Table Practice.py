2258b4e1e6d8f278128f97d5861c5c06a24835d4
GustiG <augustin_gurgulescu@yahoo.com>
2024-08-08 18:06:29 +0300

    Hash Table implementation practice

--- 5 Hash Tables/Hash-Table Practice.py
+++ 5 Hash Tables/Hash-Table Practice.py
@@ -1747,7 +1747,7 @@ print(my_hash_table.keys())
 ################################################################
 ################################################################
 ################################################################
-
+'''
 class HashTable:
     def __init__(self, size = 7):
         self.data_map = [None] * size
@@ -1792,3 +1792,53 @@ ht.set_item("Fifi", 3)          # 1 in memory
 ht.set_item("Gigi", 8)          # 5 in memory
 ht.print_table()
 print(ht.get_item("Mimi"))
+'''
+################################################################
+################################################################
+################################################################
+
+class HashTable:
+    def __init__(self, size = 7):
+        self.data_map = [None] * size
+
+    def __hash(self, key):
+        my_hash = 0
+        for letter in key:
+            my_hash = \
+            (my_hash + ord(letter) * 23) % len(self.data_map)
+        return my_hash
+    
+    def print_table(self):
+        for i, val in enumerate(self.data_map):
+            print(i, ": ", val)
+    
+    def set_item(self, key, value):
+        index = self.__hash(key)
+        if self.data_map[index] == None:
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])
+    
+    def get_item(self, key):
+        index = self.__hash(key)
+        if self.data_map[index]:
+            for i in range(len(self.data_map)):
+                if self.data_map[index][i][0] == key:
+                    return self.data_map[index][i][1]
+        return None
+    
+    def keys(self):
+        all_keys = []
+        for i in range(len(self.data_map)):
+            if self.data_map[i]:
+                for j in range(len(self.data_map[i])):
+                    all_keys.append(self.data_map[i][j][0])
+        return all_keys
+    
+my_hash_table = HashTable()
+my_hash_table.set_item('bolts', 1000)
+my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 70)
+my_hash_table.print_table()
+print(my_hash_table.get_item('washers'))
+print(my_hash_table.get_item('Washers'))
+print(my_hash_table.keys())
