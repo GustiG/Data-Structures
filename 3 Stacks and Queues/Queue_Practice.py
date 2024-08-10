319768fbbfedaeeea1a69cb969b2e7f4d0247dc6
GustiG <augustin_gurgulescu@yahoo.com>
2024-08-10 15:07:25 +0300

    Queue implementation practice

--- 3 Stacks and Queues/Queue_Practice.py
+++ 3 Stacks and Queues/Queue_Practice.py
@@ -312,7 +312,7 @@ q.print_queue()
 ###############################################################
 ###############################################################
 ###############################################################
-
+'''
 class Node:
     def __init__(self, value):
         self.value = value
@@ -345,7 +345,7 @@ class Queue:
     def dequeue(self):
         if self.length == 0:
             return None
-        temp         = self.first
+        temp = self.first
         if self.length == 1:
             self.first = None
             self.last  = None
@@ -355,6 +355,61 @@ class Queue:
         self.length -= 1
         return temp
     
+
+q = Queue(0)
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+q.dequeue()
+q.print_queue()
+'''
+###############################################################
+###############################################################
+###############################################################
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+
+class Queue:
+    def __init__(self, value):
+        new_node    = Node(value)
+        self.first  = new_node
+        self.last   = new_node
+        self.length = 1
+
+    def print_queue(self):
+        temp = self.first
+        while temp:
+            print(f'{temp.value}', end=' ')
+            temp = temp.next
+    
+    def enqueue(self, value):
+        new_node = Node(value)
+        if not self.first:
+            self.first = new_node
+            self.last  = new_node
+        else:
+            self.last.next = new_node
+            self.last      = new_node
+        self.length += 1
+        
+    def dequeue(self):
+        if self.length == 0:
+            return None
+        temp = self.first
+        if self.length == 1:
+            self.first = None
+            self.last  = None
+        else:
+            self.first   = self.first.next
+            temp.next    = None
+        self.length -= 1
+        return temp       
+
+
 
 q = Queue(0)
 q.enqueue(1)
