b03eca72fbeda305cfd5fd834614e4f11c0c988f
GustiG <augustin_gurgulescu@yahoo.com>
2024-08-15 18:44:51 +0300

    HT: Item in common

--- 5 Hash Tables/HT-ItemInCommon2.py
+++ 5 Hash Tables/HT-ItemInCommon2.py
@@ -2,12 +2,12 @@ def item_in_common(list1, list2):
     my_dict = {}
     for i in list1:
         my_dict[i] = True
-
+    
     for j in list2:
         if j in my_dict:
             return True
-
     return False
+    
 
 
 list1 = [1,3,5]
