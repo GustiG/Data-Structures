b34e5570165ca1f45ad26bbe009be67b2ec31d9f
GustiG <augustin_gurgulescu@yahoo.com>
2024-08-15 15:11:02 +0300

    Binary Search Tree: Implementation

--- 4 Trees/Binary_Search_Trees_Practice.py
+++ 4 Trees/Binary_Search_Trees_Practice.py
@@ -1341,7 +1341,7 @@ print(bst.contains(5))
 ##############################################################
 ##############################################################
 ##############################################################
-
+'''
 class Node:
     def __init__(self, value):
         self.value = value
@@ -1353,6 +1353,64 @@ class BinarySearchTree:
     def __init__(self):
         self.root = None
     
+    def insert(self, value):
+        new_node = Node(value)
+        if not self.root:
+            self.root = new_node
+            return True
+        temp = self.root
+        while True:
+            if new_node.value == temp.value:
+                return False
+            elif new_node.value < temp.value:
+                if not temp.left:
+                    temp.left = new_node
+                    return True
+                else:
+                    temp = temp.left
+            else:
+                if not temp.right:
+                    temp.right = new_node
+                    return True
+                else:
+                    temp = temp.right
+    
+    def contains(self, value):
+        temp = self.root
+        while temp:
+            if value < temp.value:
+                temp = temp.left
+            elif value > temp.value:
+                temp = temp.right
+            else:
+                return True
+        return False
+    
+
+bst = BinarySearchTree()
+bst.insert(4)
+bst.insert(3)
+bst.insert(2)
+bst.insert(1)
+bst.insert(5)
+print(bst.root.left.left.left.value)
+print(bst.contains(5))
+'''
+##############################################################
+##############################################################
+##############################################################
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left  = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
     def insert(self, value):
         new_node = Node(value)
         if not self.root:
