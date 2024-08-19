24e0ae5211633ddc86101f9cb5e86f8158d3568e
GustiG <augustin_gurgulescu@yahoo.com>
2024-08-19 20:39:35 +0300

    Hash Table: Item in common

--- 5 Hash Tables/HT-ItemInCommon2.py
+++ 5 Hash Tables/HT-ItemInCommon2.py
@@ -1,13 +1,14 @@
 def item_in_common(list1, list2):
-    my_dict = {}
+    my_dic = {}
+
     for i in list1:
-        my_dict[i] = True
+        my_dic[i] = True
     
     for j in list2:
-        if j in my_dict:
+        if j in my_dic:
             return True
+
     return False
-    
 
 
 list1 = [1,3,5]
@@ -16,3 +17,15 @@ list2 = [2,4,5]
 
 print(item_in_common(list1, list2))
 # since 5 is in both lists, it should return True
+
+"""
+SOLUTION using a SET:
+
+    my_set = set(list1)    
+
+    for i in list2:
+        if i in my_set:
+            return True
+    return False
+
+"""
