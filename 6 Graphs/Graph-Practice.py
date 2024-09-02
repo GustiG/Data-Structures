501d44433be71c26facfa1e7e85f1f3ffbcfea0e
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-02 15:54:07 +0300

    Graph: Implementation

--- 6 Graphs/Graph-Practice.py
+++ 6 Graphs/Graph-Practice.py
@@ -197,10 +197,21 @@ class Graph:
         return False
 
 
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and \
+        v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+
+
 my_graph = Graph()
 my_graph.add_vertex('A')
+my_graph.add_vertex('G')
+my_graph.add_edge('A', 'G')
 my_graph.print_graph()
-
 #'''
 
 ########################################################
