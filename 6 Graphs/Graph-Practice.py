947284e1b7dee1d991395f0e2f2ec5e521a0ea15
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-02 14:56:55 +0300

    Graph: Implementation

--- 6 Graphs/Graph-Practice.py
+++ 6 Graphs/Graph-Practice.py
@@ -1,3 +1,4 @@
+        # Graph implementation using Adjacency List
 '''
 class Graph:
     def __init__(self):
@@ -26,7 +27,7 @@ my_graph.print_graph()
 ########################################################
 ########################################################
 
-#'''
+'''
 class Graph:
     def __init__(self):
         self.adj_list = {}
@@ -47,4 +48,31 @@ class Graph:
 my_graph = Graph()
 my_graph.add_vertex('G')
 my_graph.add_vertex('A')
+my_graph.print_graph()
+'''
+
+########################################################
+########################################################
+########################################################
+
+#'''
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+    
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+
+my_graph = Graph()
+my_graph.add_vertex('G')
 my_graph.print_graph()
