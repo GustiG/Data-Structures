c77b29c43cb306096d17b8ea78ebe4a038d5b20f
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-03 00:08:44 +0300

    Graph: Implementation

--- 6 Graphs/Graph-Practice.py
+++ 6 Graphs/Graph-Practice.py
@@ -575,7 +575,7 @@ my_graph.print_graph()
 ########################################################
 ########################################################
 
-#'''
+'''
 class Graph:
     def __init__(self):
         self.adj_list = {}
@@ -641,4 +641,145 @@ my_graph.add_edge('C', 'D')
 my_graph.remove_edge('A', 'C')
 my_graph.remove_vertex('D')
 my_graph.print_graph()
+'''
+
+########################################################
+########################################################
+########################################################
+
+'''
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+    
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.key():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and \
+        v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)        
+            self.adj_list[v2].append(v1)        
+            return True
+        return False
+    
+
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and \
+        v2 in self.adj_list.keys():
+           try:
+               self.adj_list[v1].remove(v2) 
+               self.adj_list[v2].remove(v1)
+           except ValueError:
+               pass
+           return True
+        return False
+    
+
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for nei_vertex in self.adj_list[vertex]:
+                self.adj_list[nei_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+    
+
+
+my_graph = Graph()
+
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('A', 'C')
+my_graph.add_edge('A', 'D')
+my_graph.add_edge('B', 'D')
+my_graph.add_edge('C', 'D')
+
+my_graph.remove_edge('A', 'C')
+my_graph.remove_vertex('D')
+my_graph.print_graph()
+'''
+
+########################################################
+########################################################
+########################################################
+
 #'''
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+
+    def add_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and \
+        v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and \
+        v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+    
+
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for nei_vertex in self.adj_list[vertex]:
+                self.adj_list[nei_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
+
+
+
+my_graph = Graph()
+
+my_graph.add_vertex('A')
+my_graph.add_vertex('B')
+my_graph.add_vertex('C')
+my_graph.add_vertex('D')
+
+my_graph.add_edge('A', 'B')
+my_graph.add_edge('A', 'C')
+my_graph.add_edge('A', 'D')
+my_graph.add_edge('B', 'D')
+my_graph.add_edge('C', 'D')
+
+my_graph.remove_edge('A', 'C')
+my_graph.remove_vertex('D')
+my_graph.print_graph()
