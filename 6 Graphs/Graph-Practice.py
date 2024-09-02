1027b3defeb68ff0cb866df4d53c9d41396576e0
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-02 15:19:21 +0300

    Graph: Implementation

--- 6 Graphs/Graph-Practice.py
+++ 6 Graphs/Graph-Practice.py
@@ -55,7 +55,7 @@ my_graph.print_graph()
 ########################################################
 ########################################################
 
-#'''
+'''
 class Graph:
     def __init__(self):
         self.adj_list = {}
@@ -76,3 +76,118 @@ class Graph:
 my_graph = Graph()
 my_graph.add_vertex('G')
 my_graph.print_graph()
+'''
+
+########################################################
+########################################################
+########################################################
+
+'''
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+    
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+    
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+        
+my_graph = Graph()
+my_graph.add_vertex('G')
+my_graph.print_graph()
+'''
+
+########################################################
+########################################################
+########################################################
+
+'''
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+    
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+
+my_graph = Graph()
+my_graph.add_vertex('G')
+my_graph.add_vertex(0)
+my_graph.print_graph()
+'''
+
+########################################################
+########################################################
+########################################################
+
+'''
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+    
+
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+
+my_graph = Graph()
+my_graph.add_vertex('G')
+my_graph.print_graph()
+'''
+
+########################################################
+########################################################
+########################################################
+
+#'''
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+
+    def print_graph(self):
+        for vertex in self.adj_list:
+            print(vertex, ":", self.adj_list[vertex])
+
+    
+    def add_vertex(self, vertex):
+        if vertex not in self.adj_list.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+
+
+my_graph = Graph()
+my_graph.add_vertex('A')
+my_graph.print_graph()
+
+#'''
+
+########################################################
+########################################################
+########################################################
