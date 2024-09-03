eb6d4d7d2466b8acb8d56e572bd28b02ccbd54ef
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-03 14:50:22 +0300

    Heap: 0 index max heap

--- 7 Heaps/Heap-Practice.py
+++ 7 Heaps/Heap-Practice.py
@@ -103,7 +103,7 @@ print(my_heap.heap)
 #################################################################
 #################################################################
 
-#'''
+'''
 class MaxHeap:
     def __init__(self):
         self.heap = []
@@ -150,4 +150,176 @@ print(my_heap.heap)
 my_heap.insert(75)
 print(my_heap.heap)
 
+'''
+
+#################################################################
+#################################################################
+#################################################################
+
+'''
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+
+    def _left_child(self, index):
+        return index * 2 + 1
+    
+
+    def _right_child(self, index):
+        return index * 2 + 2
+
+
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = \
+        self.heap[index2], self.heap[index1]
+
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and \
+        self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+
+
+my_heap = MaxHeap()
+my_heap.insert(99)
+my_heap.insert(72)
+my_heap.insert(61)
+my_heap.insert(58)    
+print(my_heap.heap)
+
+my_heap.insert(100)
+print(my_heap.heap)
+
+my_heap.insert(75)
+print(my_heap.heap)
+
+'''
+
+#################################################################
+#################################################################
+#################################################################
+
+'''
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+
+    def _left_child(self, index):
+        return index * 2 + 1
+    
+
+    def _right_child(self, index):
+        return index * 2 + 2
+    
+
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = \
+        self.heap[index2], self.heap[index1]
+
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and \
+        self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+
+
+my_heap = MaxHeap()
+my_heap.insert(99)
+my_heap.insert(72)
+my_heap.insert(61)
+my_heap.insert(58)    
+print(my_heap.heap)
+
+my_heap.insert(100)
+print(my_heap.heap)
+
+my_heap.insert(75)
+print(my_heap.heap)
+'''
+
+#################################################################
+#################################################################
+#################################################################
+
+'''
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+
+    def _left_child(self, index):
+        return index * 2 + 1
+    
+
+    def _right_child(self, index):
+        return index * 2 + 2
+    
+
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = \
+        self.heap[index2], self.heap[index1]
+
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and \
+        self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+
+
+my_heap = MaxHeap()
+my_heap.insert(99)
+my_heap.insert(72)
+my_heap.insert(61)
+my_heap.insert(58)    
+print(my_heap.heap)
+
+my_heap.insert(100)
+print(my_heap.heap)
+
+my_heap.insert(75)
+print(my_heap.heap)
+'''
+
+#################################################################
+#################################################################
+#################################################################
+
 #'''
+
+
+
+#'''
+
+#################################################################
+#################################################################
+#################################################################
