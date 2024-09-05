4bf045ef00d65a34fc8c8cdf93e13369ce8dfe04
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-05 12:27:41 +0300

    Heap: 0 index, max heap

--- 7 Heaps/Heap-Practice.py
+++ 7 Heaps/Heap-Practice.py
@@ -1308,7 +1308,7 @@ print(myHeap.heap)
 #################################################################
 #################################################################
 
-#'''
+'''
 class MaxHeap:
     def __init__(self):
         self.heap = []
@@ -1356,6 +1356,83 @@ class MaxHeap:
                 return
             
 
+    def remove(self):
+        if len(self.heap) == 0: return None
+        if len(self.heap) == 1: return self.heap.pop()
+
+        maxValue = self.heap[0]
+        self.heap[0] = self.heap.pop()
+
+        self._sink_down(0)
+        return maxValue
+
+
+
+myHeap = MaxHeap()
+myHeap.insert(12)
+myHeap.insert(23)
+myHeap.insert(44)
+myHeap.insert(17)
+myHeap.insert(31)
+myHeap.insert(88)
+print(myHeap.heap)
+
+myHeap.remove()
+print(myHeap.heap)
+'''
+
+#################################################################
+#################################################################
+#################################################################
+
+#'''
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index): return index * 2 + 1
+
+    def _right_child(self, index): return index * 2 + 2
+
+    def _parent(self, index): return (index - 1) // 2
+
+    def _swap(self, index1, index2): 
+        self.heap[index1], self.heap[index2] =\
+        self.heap[index2], self.heap[index1]
+
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and\
+        self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+
+    def _sink_down(self, index):
+        maxIndex = index
+
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            if left_index < len(self.heap) and\
+            self.heap[left_index] > self.heap[maxIndex]:
+                maxIndex = left_index
+
+            if right_index < len(self.heap) and\
+            self.heap[right_index] > self.heap[maxIndex]:
+                maxIndex = right_index
+
+            if maxIndex != index:
+                self._swap(index, maxIndex)
+                index = maxIndex
+            else:
+                return
+            
+        
     def remove(self):
         if len(self.heap) == 0: return None
         if len(self.heap) == 1: return self.heap.pop()
