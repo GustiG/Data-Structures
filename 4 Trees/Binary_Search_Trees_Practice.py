1e4e87817755fdde3b5cbf56d376a10917fd0eea
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-06 14:10:48 +0300

    rBST

--- 4 Trees/Binary_Search_Trees_Practice.py
+++ 4 Trees/Binary_Search_Trees_Practice.py
@@ -2105,38 +2105,61 @@ class BinarySearchTree:
 
 
     def __r_contains(self, current_node, value):
-        if current_node == None:
+        if current_node == None: 
             return False
-        if value == current_node.value:
+        if current_node.value == value:
             return True
-        
+
         if value < current_node.value:
             return self.__r_contains(current_node.left, value)
         if value > current_node.value:
             return self.__r_contains(current_node.right, value)
-        
+
 
     def r_contains(self, value):
         return self.__r_contains(self.root, value)
     
 
 
+    def __r_insert(self, current_node, value):
+        if current_node == None:
+            return Node(value)    # creates the new node
+        
+        if value < current_node.value:
+            current_node.left =\
+            self.__r_insert(current_node.left, value)
+
+        if value > current_node.value:
+            current_node.right =\
+            self.__r_insert(current_node.right, value)
+        
+        return current_node # the parent node until the root
+
+
+    def r_insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
+
+        
+
 my_tree = BinarySearchTree()
-my_tree.insert(47)
-my_tree.insert(21)
-my_tree.insert(76)
-my_tree.insert(18)
-my_tree.insert(27)
-my_tree.insert(25)
-my_tree.insert(82)
+my_tree.r_insert(47)
+my_tree.r_insert(21)
+my_tree.r_insert(76)
+my_tree.r_insert(18)
+my_tree.r_insert(27)
+my_tree.r_insert(25)
+my_tree.r_insert(82)
 
 print("BST Contains 27:")
 print(my_tree.r_contains(27))
 
-print("\nBST Contains 22:")
-print(my_tree.r_contains(22))
+print("\nBST Contains 17:")
+print(my_tree.r_contains(17))
 
 #'''
 ##############################################################
 ##############################################################
 ##############################################################
+ 
