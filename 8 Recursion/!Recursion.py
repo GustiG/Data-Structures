4aeeba1d3b5240faa3ad8557873056d79d158527
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-06 12:41:32 +0300

    Recursion: examples & practice

--- 8 Recursion/!Recursion.py
+++ 8 Recursion/!Recursion.py
@@ -1,3 +1,5 @@
+        # Time complexity: O(<times its called> ^ n)
+
 # def sum_n(n):
 #     if n == 0:
 #         return 0
@@ -29,7 +31,7 @@
 #         print(i + 1, end=" ") # i + 1 to avoid printing zeroes
 #     print()
 
-#     pattern_print(n - 1)   # move this at the beginning of the function 
+#     pattern_print(n - 1)   # move this above the loop 
 #                             # to flip the pattern
     
 # pattern_print(8)
@@ -171,7 +173,7 @@ pp(4)
 '''
 
 ##############################################################
-
+'''
 def pp(n):
     if n == 1:
         print(1)
@@ -189,3 +191,68 @@ def pp(n):
 
 
 pp(4)
+'''
+
+##############################################################
+'''
+def sayHello(n):
+    if n == 0:
+        return
+    
+    sayHello(n - 1)         # move this line under print to 
+    print("Hello", n)       # count backwards
+    
+
+sayHello(4)
+'''
+
+##############################################################
+'''
+def addDigits(n):
+    if n == 0:
+        return 0
+    
+    last_digit = n % 10
+    remaining = n // 10
+
+    print(remaining, "|", last_digit)
+    return addDigits(remaining) + last_digit
+
+
+print(addDigits(13141))
+'''
+
+##############################################################
+'''
+def factorial(n):
+    if n == 1: return 1
+
+    return n * factorial(n - 1)
+
+
+print(factorial(4))
+'''
+
+##############################################################
+'''
+def numSum(n):
+    if n == 0: return 0
+
+    return n + numSum(n - 1)
+
+
+print(numSum(10))
+'''
+
+# made the above function without recursion because it's great:
+'''
+def numSum(n):
+    half = n // 2
+    firstPlusLast = 1 + n
+    result = half * firstPlusLast
+    return result
+
+print(numSum(10))
+'''
+
+##############################################################
