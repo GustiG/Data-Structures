23db4c21c43186ffae05dda408ae1b54fb7e71fd
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-10 18:55:41 +0300

    BST: BFS

--- 4 Trees/Binary_Search_Trees_Practice.py
+++ 4 Trees/Binary_Search_Trees_Practice.py
@@ -2142,6 +2142,24 @@ class BinarySearchTree:
         self.__r_insert(self.root, value)
 
         
+    def BFS(self):
+        current_node = self.root
+        queue = []
+        results = []
+        queue.append(current_node)
+
+        while len(queue) > 0:
+            current_node = queue.pop(0)
+            results.append(current_node.value)
+            if current_node.left is not None:
+                queue.append(current_node.left)
+            if current_node.right is not None:
+                queue.append(current_node.right)
+        return results
+             
+
+
+
 
 my_tree = BinarySearchTree()
 my_tree.r_insert(47)
@@ -2149,14 +2167,15 @@ my_tree.r_insert(21)
 my_tree.r_insert(76)
 my_tree.r_insert(18)
 my_tree.r_insert(27)
-my_tree.r_insert(25)
+my_tree.r_insert(52)
 my_tree.r_insert(82)
 
-print("BST Contains 27:")
-print(my_tree.r_contains(27))
+print(my_tree.BFS())
+# print("BST Contains 27:")
+# print(my_tree.r_contains(27))
 
-print("\nBST Contains 17:")
-print(my_tree.r_contains(17))
+# print("\nBST Contains 17:")
+# print(my_tree.r_contains(17))
 
 #'''
 ##############################################################
