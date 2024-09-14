d8af6d7f3cf608d05fa9134d490ffc5e34ff6df9
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-15 00:02:09 +0300

    BST: Kth smallest

--- 4 Trees/BST Kth Smallest Node.py
+++ 4 Trees/BST Kth Smallest Node.py
@@ -49,17 +49,17 @@ class BinarySearchTree:
     #                                #
     ##################################
     def kth_smallest(self, k):
-        nodes = []
+        values = []
         def traverse(node):
             if node is None: return
             if node.left:
                 traverse(node.left)
-            nodes.append(node.value)
+            values.append(node.value)
             if node.right:
                 traverse(node.right)
         traverse(self.root)
         
-        return nodes[k-1] # because we start at 1 index here
+        return values[k-1] # because we start at 1 index here
 
 
 
