f5f2082020e633d3292cc6a2a39f8d7635ff05a6
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-27 15:10:01 +0300

    Simple Sorts: Bubble sort

--- 9 Basic Sorts/1. Bubble Sort.py
+++ 9 Basic Sorts/1. Bubble Sort.py
@@ -1,4 +1,5 @@
-
+                # Time complexity: O(n^2) : worst case, if the list is reversed
+'''
 def bubble_sort(my_list):
     for i in range(len(my_list) -1, 0, -1):
         for j in range(i):
@@ -9,3 +10,15 @@ def bubble_sort(my_list):
     return my_list
 
 print(bubble_sort([4, 2, 6, 5, 1, 3]))
+'''
+
+#'''
+def bs(arr):  # As the first sorted element in the list will be the last one, it                        
+    for i in range(len(arr) -1, 0, -1):  # iterates over the list in reverse to
+        for j in range(i):          # gradually reduce the elements to be sorted  
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+print(bs([5, 2, 7, 1, 4]))
+#'''
