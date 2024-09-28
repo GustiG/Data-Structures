9cb76ffd355f29041e559fab8510a5f6144d6ef3
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-28 20:06:14 +0300

    BST

--- 4 Trees/Binary_Search_Trees_Practice.py
+++ 4 Trees/Binary_Search_Trees_Practice.py
@@ -2415,9 +2415,9 @@ class BinarySearchTree:
         def traverse(node):
             if node.left:
                 traverse(node.left)
-            results.append(node.value)
             if node.right:
                 traverse(node.right)
+            results.append(node.value)
         traverse(self.root)
         return results
 
@@ -2427,9 +2427,9 @@ class BinarySearchTree:
         def traverse(node):
             if node.left:
                 traverse(node.left)
+            results.append(node.value)
             if node.right:
                 traverse(node.right)
-            results.append(node.value)
         traverse(self.root)
         return results
 
