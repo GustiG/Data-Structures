813e1febc4c3184fd22731c9a890889a212f3955
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-29 19:36:50 +0300

    Simple Sorts: Bubble Sort

--- 9 Basic Sorts/1. Bubble Sort.py
+++ 9 Basic Sorts/1. Bubble Sort.py
@@ -23,6 +23,7 @@ print(bubble_sort([4, 2, 6, 5, 1, 3]))
 # print(bs([5, 2, 7, 1, 4]))
 #'''
 
+
 #'''
 # def bubble_sort(my_list):
 #     for i in range(len(my_list) -1, 0, -1):
@@ -50,13 +51,41 @@ def bubble_sort(my_list):
 print(bubble_sort([5, 3, 1, 6, 4, 2]))
 '''
 
-#'''
+
+'''
 def bubble_sort(my_list):
     for i in range(len(my_list) -1, 0, -1):
         for j in range(i):
             if my_list[j] > my_list[j + 1]:
                 temp           = my_list[j]
                 my_list[j]     = my_list[j + 1]
+                my_list[j + 1] = temp
+    return my_list
+
+print(bubble_sort([5, 3, 1, 6, 4, 2]))
+'''
+
+
+'''
+def bubble_sort(my_list):
+    for i in range(len(my_list) -1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j + 1]:
+                my_list[i], my_list[j] =\
+                my_list[j], my_list[i]
+    return my_list
+
+print(bubble_sort([5, 3, 1, 6, 4, 2]))
+'''
+
+
+#'''
+def bubble_sort(my_list):
+    for i in range(len(my_list) -1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
                 my_list[j + 1] = temp
     return my_list
 
