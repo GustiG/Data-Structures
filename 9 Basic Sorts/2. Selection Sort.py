06fb21f8992cbceb61efab899b27fb78b1725554
GustiG <augustin_gurgulescu@yahoo.com>
2024-09-29 19:27:42 +0300

    Simple Sorts: Selection Sort

--- 9 Basic Sorts/2. Selection Sort.py
+++ 9 Basic Sorts/2. Selection Sort.py
@@ -49,7 +49,7 @@ print(selection_sort([1, 4, 3, 5, 6, 2]))
 '''
 
 
-#'''
+'''
 def selection_sort(my_list):
     for i in range(len(my_list) -1):
         min_index = i
@@ -59,6 +59,23 @@ def selection_sort(my_list):
         if i != min_index:
             my_list[i], my_list[min_index] =\
             my_list[min_index], my_list[i]
+    return my_list
+
+print(selection_sort([1, 4, 3, 5, 6, 2]))
+'''
+
+
+#'''
+def selection_sort(my_list):
+    for i in range(len(my_list) -1):
+        min_index = i
+        for j in range(i + 1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index:
+            temp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = temp
     return my_list
 
 print(selection_sort([1, 4, 3, 5, 6, 2]))
