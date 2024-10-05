91d2c005d336ad65e4ba9a62025f752cbca933fd
GustiG <augustin_gurgulescu@yahoo.com>
2024-10-05 15:40:02 +0300

    Basic Sorts: Bubble Sort

--- 9 Basic Sorts/1. Bubble Sort.py
+++ 9 Basic Sorts/1. Bubble Sort.py
@@ -93,6 +93,19 @@ print(bubble_sort([5, 3, 1, 6, 4, 2]))
 '''
 
 
+'''
+def bubble_sort(my_list):
+    for i in range(len(my_list) -1, 0, -1):
+        for j in range(i):
+            if my_list[j] > my_list[j + 1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j + 1]
+                my_list[j + 1] = temp
+    return my_list
+
+print(bubble_sort([5, 3, 1, 6, 4, 2]))
+'''
+
 #'''
 def bubble_sort(my_list):
     for i in range(len(my_list) -1, 0, -1):
