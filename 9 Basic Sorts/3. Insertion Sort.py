6304f52e740c3f2a507151b1da2e55991d51535d
GustiG <augustin_gurgulescu@yahoo.com>
2024-10-05 20:22:18 +0300

    Basic Sorts: Insertion Sort

--- 9 Basic Sorts/3. Insertion Sort.py
+++ 9 Basic Sorts/3. Insertion Sort.py
@@ -30,7 +30,7 @@ print(insertion_sort([6, 2, 4, 3, 5, 1]))
 '''
 
 
-#'''
+'''
 def insertion_sort(my_list):
     for i in range(1, len(my_list)):
         temp = my_list[i]
@@ -41,6 +41,81 @@ def insertion_sort(my_list):
             j -= 1
     return my_list
 
+
+print(insertion_sort([6, 2, 4, 3, 5, 1]))
+'''
+
+
+'''
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i-1
+        while temp < my_list[j] and j > -1:
+            my_list[j+1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+print(insertion_sort([6, 2, 4, 3, 5, 1]))
+'''
+
+
+'''
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j + 1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+print(insertion_sort([6, 2, 4, 3, 5, 1]))
+'''
+
+
+'''
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j + 1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+print(insertion_sort([6, 2, 4, 3, 5, 1]))
+'''
+
+
+'''
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j + 1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
+
+print(insertion_sort([6, 2, 4, 3, 5, 1]))
+'''
+
+
+#'''
+def insertion_sort(my_list):
+    for i in range(1, len(my_list)):
+        temp = my_list[i]
+        j = i - 1
+        while temp < my_list[j] and j > -1:
+            my_list[j + 1] = my_list[j]
+            my_list[j] = temp
+            j -= 1
+    return my_list
 
 print(insertion_sort([6, 2, 4, 3, 5, 1]))
 #'''
