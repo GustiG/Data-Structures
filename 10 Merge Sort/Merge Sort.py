576c6d8d643fd1d128cd8dc783a6783c6428a22b
GustiG <augustin_gurgulescu@yahoo.com>
2024-10-10 14:54:37 +0300

    Merge Sort

--- 10 Merge Sort/Merge Sort.py
+++ 10 Merge Sort/Merge Sort.py
@@ -1,7 +1,7 @@
 #       Merge Sort     Time Complexity: O(n log n) | Space complexity: O(n)
 #                    (log n) for dividing the array and (n) for putting it back
 
-#'''
+'''
 def merge(list1, list2):
     combined = []
     i, j = 0, 0
@@ -34,4 +34,68 @@ def merge_sort(my_list):
 
 
 print(merge_sort([3, 1, 4, 2]))
+'''
+
+
+'''
+def merge(list1, list2):
+    combined = []
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
+
+
+def merge_sort(my_list):
+    if len(my_list) == 1: return my_list
+    mid_index = int(len(my_list) / 2)
+    left = merge_sort(my_list[:mid_index])
+    right = merge_sort(my_list[mid_index:])
+    return merge(left, right)
+
+
+print(merge_sort([3, 5, 1, 0, 8]))
+'''
+
+
+#'''
+def merge(list1, list2):
+    combined = []
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+    return combined
+
+
+def merge_sort(my_list):
+    if len(my_list) == 1: return my_list
+    mid_index = int(len(my_list) / 2)
+    left = merge_sort(my_list[:mid_index])
+    right = merge_sort(my_list[mid_index:])
+    return merge(left, right)
+
+
+print(merge_sort([3, 5, 1, 0, 8]))
 #'''
