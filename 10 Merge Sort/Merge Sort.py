8ea64c3a47b811f97cfe53b2218c8dfd830015b9
GustiG <augustin_gurgulescu@yahoo.com>
2024-10-12 15:40:39 +0300

    Merge Sort

--- 10 Merge Sort/Merge Sort.py
+++ 10 Merge Sort/Merge Sort.py
@@ -133,7 +133,7 @@ def merge_sort(my_list):
 print(merge_sort([3, 5, 1, 0, 8]))
 '''
 
-#'''
+'''
 def merge(list1, list2):
     combined = []
     i, j = 0, 0
@@ -159,6 +159,38 @@ def merge_sort(my_list):
     left = merge_sort(my_list[:mid_index])
     right = merge_sort(my_list[mid_index:])
     return merge(left, right)
+
+
+print(merge_sort([3, 5, 1, 0, 8]))
+'''
+
+
+#'''
+def merge(list1, list2):
+    combined = []
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 2
+    return combined
+
+
+def merge_sort(my_list):
+    if len(my_list) == 1: return my_list
+    mid_index = int(len(my_list) / 2)
+    left = merge_sort(my_list[: mid_index])
+    right = merge_sort(my_list[mid_index :])
+    return merge(left, right)    
 
 
 print(merge_sort([3, 5, 1, 0, 8]))
