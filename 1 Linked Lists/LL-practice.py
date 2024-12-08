4b976cc97664ca2eb3f8f49c8c887e6081a6c25d
GustiG <augustin_gurgulescu@yahoo.com>
2024-12-08 16:08:09 +0200

    Linked List implementation practice

--- 1 Linked Lists/LL-practice.py
+++ 1 Linked Lists/LL-practice.py
@@ -6589,7 +6589,7 @@ ll.print_list()
 #################################################################
 #################################################################
 
-#'''
+'''
 class Node:
     def __init__(self, value):
         self.value = value
@@ -6739,4 +6739,163 @@ ll.remove(1)
 ll.reverse()
 ll.print_list()
 
+'''
+
+#################################################################
+#################################################################
+#################################################################
+
 #'''
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next  = None
+
+
+class LinkedList:
+    def __init__(self, value):
+        new_node    = Node(value)
+        self.head   = new_node
+        self.tail   = new_node
+        self.length = 1
+
+
+    def print_list(self):
+        temp = self.head
+        while temp:
+            print(f'{temp.value}', end = ' ')
+            temp = temp.next
+    
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail      = self.tail.next
+        self.length += 1
+        return True
+
+    
+    def pop(self):
+        if self.length == 0:
+            return None
+        prev = self.head
+        temp = self.head
+        while temp.next:
+            prev = temp
+            temp = temp.next
+        self.tail = prev
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
+
+
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head     = new_node
+        self.length += 1
+        return True
+
+
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp      = self.head
+        self.head = self.head.next
+        temp.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.tail = None
+        return temp
+
+
+    def get(self, index):
+        if index == 0 or index >= self.length:
+             return None
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+
+
+    def set_value(self, index, value):
+        temp = self.get(index)    
+        if temp:
+            temp.value = value
+            return True
+        return False
+
+
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node      = Node(value)
+        temp          = self.get(index -1)
+        new_node.next = temp.next
+        temp.next     = new_node
+        self.length  += 1
+        return True
+
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index -1)
+        temp = prev. next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+
+ll = LinkedList(3)
+ll.append(2)
+ll.append(2)
+ll.pop()
+ll.prepend(4)
+ll.prepend(4)
+ll.pop_first()
+ll.set_value(2, 'X')
+ll.insert(2, 0)
+ll.remove(3)
+ll.reverse()
+ll.print_list()
+# print(ll.get(1))
+
+#'''
+
+#################################################################
+#################################################################
+#################################################################
